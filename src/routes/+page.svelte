0b29e32fc796c6b6cd137d617568ca412affedc7
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-18 23:40:54 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<div class="container">
+  <div class="container__card">
+    <h1>Get <span>insights</span> that help your business grow.</h1>
+    <p>
+      Discover the benefits of data analytics and make better decisions
+      regarding revenue, customer experience, and overall efficiency.
+    </p>
+  </div>
+</div>
+
+<style>
+  .container {
+    background-color: hsl(244, 38%, 16%);
+    background-image: url('https://dimmageiras.github.io/frontend-mentor-stats-preview-card-component/images/image-header-desktop.jpg');
+    background-position: right;
+    background-repeat: no-repeat;
+    background-size: 50%;
+    border-radius: 0.5rem;
+    width: 111.2rem;
+    height: 45.4rem;
+    margin: 17.8rem auto;
+  }
+
+  span {
+    color: hsl(277, 64%, 61%);
+  }
+  .container__card {
+    display: flex;
+    flex-direction: column;
+    color: #fff;
+  }
+</style>
